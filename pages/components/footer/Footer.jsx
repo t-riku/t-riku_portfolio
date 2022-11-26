9f4bdfee370e99f832eb1fb76a11d57f61db7a93
t-riku <dingariku@icloud.com>
2022-11-26 16:29:59 +0900

    ✨ Footerコンポーネントを追加

---
+++ pages/components/footer/Footer.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import classes from "./footer.module.css";
+import { FaFacebookF } from "react-icons/fa";
+import { FiInstagram } from "react-icons/fi";
+import { IoLogoTwitter } from "react-icons/io";
+
+const Footer = () => {
+  return (
+    <footer className={classes.footer}>
+      <a href="#" className={classes.footer__logo}>
+        EGATOR
+      </a>
+
+      <ul className={classes.permalinks}>
+        <li>
+          <a href="#">HOME</a>
+        </li>
+        <li>
+          <a href="#about">ABOUT</a>
+        </li>
+        <li>
+          <a href="#experience">EXPERIENCE</a>
+        </li>
+        <li>
+          <a href="#services">SERVICES</a>
+        </li>
+        <li>
+          <a href="#portfolio">PORTFOLIO</a>
+        </li>
+        <li>
+          <a href="#testimonials">TESTIMONIALS</a>
+        </li>
+        <li>
+          <a href="#contact">CONTACT</a>
+        </li>
+      </ul>
+
+      <div className={classes.footer__socials}>
+        <a href="https://facebook.com">
+          <FaFacebookF />
+        </a>
+        <a href="https://inatagram.com">
+          <FiInstagram />
+        </a>
+        <a href="https://twitter.com">
+          <IoLogoTwitter />
+        </a>
+      </div>
+
+      <div className={classes.footer__copyright}>
+        <small>&copy; t-riku Portfolio Demo Site. All Right Reserved.</small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,10 @@ import Nav from "./components/nav/Nav";
 import About from "./components/about/About";
 // import Experience from "./components/experience/Experience";
 // import Services from "./components/services/Services";
-// import Portfolio from "./components/portfolio/Portfolio";
+import Portfolio from "./components/portfolio/Portfolio";
 // import Testimonials from "./components/testimonials/Testimonials";
 // import Contact from "./components/contact/Contact";
-// import Footer from "./components/footer/Footer";
+import Footer from "./components/footer/Footer";
 
 export default function Home() {
   return (
@@ -14,12 +14,12 @@ export default function Home() {
       <Header />
       <Nav />
       <About />
-      {/* <Experience />
-      <Services />
+      {/* <Experience /> */}
+      {/* <Services /> */}
       <Portfolio />
-      <Testimonials />
-      <Contact />
-      <Footer /> */}
+      {/* <Testimonials /> */}
+      {/* <Contact /> */}
+      <Footer />
     </div>
   );
 }
