2487a90e4536a2df5464346042bfded6f37d87ea
t-riku <dingariku@icloud.com>
2022-12-05 11:37:10 +0900

    💬 コメントを追加

--- src/components/experience/ExperienceItems.tsx
+++ src/components/experience/ExperienceItems.tsx
@@ -7,14 +7,20 @@ export type ITEM = {
   title: string;
   category: string;
   level: string;
+  // level: Basic/Advanced/Intermediate;
+  // level = "Basic" | "Advanced" | "Intermedeate"
 };
 
 const WorkItems = ({ item }: { item: ITEM }) => {
   return (
     <article className={classes.experience__details} key={item.id}>
       <BsPatchCheckFill className={classes.experience__details__icon} />
+      {/* levelの熟練度によってiconの色を変えたい。三項演算子（if文）でclassを付け替え、
+      色をかえる。{level==="Advance" ? {classes.activeBlue}:""}みたいな */}
       <div>
-        <h4>{item.title}</h4>
+        <h4>
+          {item.title} <span> / {item.category} </span>
+        </h4>
         <small>{item.level}</small>
       </div>
     </article>
