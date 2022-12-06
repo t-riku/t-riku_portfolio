81574afb8980eecb91fe051585a1bc7e383ca283
t-riku <dingariku@icloud.com>
2022-12-06 17:59:07 +0900

    🍱 imageパスを変更

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import classes from "src/components/about/About.module.css";
-import ME from "public/assets/taka.jpg";
+import ME from "public/assets/me/IMG_1304.png";
 import { FaAward } from "react-icons/fa";
 import { FiUsers } from "react-icons/fi";
 import { VscFolderLibrary } from "react-icons/vsc";
