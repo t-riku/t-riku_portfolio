ccc805a59ee195066106efbe1627da5904624f93
t-riku <dingariku@icloud.com>
2022-12-26 16:28:07 +0900

    🎨細かい修正

--- src/components/work/Works.tsx
+++ src/components/work/Works.tsx
@@ -26,9 +26,9 @@ const Works = () => {
   };
 
   return (
-    <div>
+    <>
       <p className={classes.work__text}>
-        特に見て欲しい制作物が「ポートフォリオサイト」「多田設計事務所（リメイク）」「Pokedex」です
+        特に見て欲しい制作物が「ポートフォリオサイト」「多田設計事務所（リメイク）」「Pokedex（制作中）」です
       </p>
       <div className={classes.work__filters}>
         {projectsNav.map((item, index) => {
@@ -53,7 +53,7 @@ const Works = () => {
           return <WorkItems item={item} key={item.id} />;
         })}
       </div>
-    </div>
+    </>
   );
 };
 
