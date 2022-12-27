904a22190a477d1cd289b244cad7ba455e33fd10
t-riku <dingariku@icloud.com>
2022-12-27 17:53:37 +0900

    🎨 新しいclassを追加

--- src/components/modal/WorkModal.tsx
+++ src/components/modal/WorkModal.tsx
@@ -32,7 +32,10 @@ const WorkModal = () => {
             key={index}
           >
             <div className={classes.modal}>
-              <div className={classes.modal__content} key={item.id}>
+              <div
+                className={`${classes.modal__content} ${classes.workModal__content}`}
+                key={item.id}
+              >
                 <div className={classes.modal__content__left}>
                   <div className={classes.work__description}>
                     <h3>{item.title}</h3>
